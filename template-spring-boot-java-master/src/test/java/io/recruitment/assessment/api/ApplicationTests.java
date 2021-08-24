0c5edd8ee3af7404c79a9b82b7626e2ee286efec
ela4490 <ela4490@gmail.com>
2021-08-24 20:12:43 +0200

    Added test case

--- template-spring-boot-java-master/src/test/java/io/recruitment/assessment/api/ApplicationTests.java
+++ template-spring-boot-java-master/src/test/java/io/recruitment/assessment/api/ApplicationTests.java
@@ -1,14 +1,21 @@
 package io.recruitment.assessment.api;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import io.recruitment.assessment.api.controllers.HelloController;
 
 @SpringBootTest
 class ApplicationTests {
+	
+	@Autowired
+	private HelloController controller;
 
     @Test
-    void contextLoads() {
-
+    void contextLoads() throws Exception {
+    	assertThat(controller).isNotNull();
     }
 
 }
